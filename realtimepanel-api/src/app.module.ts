723eafb24e8873ea890ab1373b2b12e743bf20fb
Sergio Pérez <sergioperezvicente6@gmail.com>
2025-09-23 14:39:08 +0200

    installing dependencies and create api modules

--- realtimepanel-api/src/app.module.ts
+++ realtimepanel-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { FilesModule } from './modules/files/files.module';
+import { WsModule } from './modules/ws/ws.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, FilesModule, WsModule],
   controllers: [AppController],
   providers: [AppService],
 })
